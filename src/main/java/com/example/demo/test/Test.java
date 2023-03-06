048814ecbf07ae0a14e7da1aa3cd543b667a2344
lnk123 <13057588205@163.com>
2023-03-07 00:08:26 +0800

    tijiao

---
+++ src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,19 @@
+package com.example.demo.test;
+
+/**
+ * Author: lnk
+ * Date : 2023 - 02 - 28 16:05
+ * Description:<描述>
+ * Version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        String str1 = "gok123";
+        String str2 = "gok";
+        String str3 = "123";
+        String str4 = str2+str3;
+
+        System.out.println(str1==str4); //false
+
+    }
+}
